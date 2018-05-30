8ae417f7c834c2e17f8a6747b3008baa8b799b0c
MIUI <1150989249@qq.com>
2018-05-30 17:57:39 +0800

    update configuration

--- src/main/java/com/java/myh/ApplicationStarter.java
+++ src/main/java/com/java/myh/ApplicationStarter.java
@@ -10,12 +10,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * @author 心安
  * @date 2018/5/29 16:21
  *
- * 配置druid必须加 @ServletComponentScan 注解，如果不加，访问页面打不开，
+ * 配置druid必须加 @ServletComponentScan 注解，如果不加，访问监控页面打不开，
  * filter和servlet、listener之类的需要单独进行注册才能使用，
  * spring boot里面提供了该注解起到注册作用
  */
 @SpringBootApplication
-@EnableJpaRepositories
+@EnableJpaRepositories(basePackages = "com.java.myh.repository")
 @ServletComponentScan
 @ComponentScan(basePackages = {"com.java.myh"})
 public class ApplicationStarter {
